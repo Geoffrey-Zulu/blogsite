a1398fb677de4151d49e1cd6d3fcaef83353a81f
Geoffrey-Zulu <76912380+Geoffrey-Zulu@users.noreply.github.com>
2024-09-03 14:14:54 +0200

    Update siteMetaData.js

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -6,16 +6,16 @@ const siteMetadata = {
     description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
     language: 'en-us',
     theme: 'system', // system, dark or light
-    siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
+    siteUrl: 'https://www.punterss.com/', // your website URL
     siteLogo: '/logo.png',
     socialBanner: '/social-banner.png', // add social banner in the public folder
-    email: 'codebucks27@gmail.com', 
-    github: 'https://github.com/codebucks27',
-    twitter: 'https://twitter.com/code_bucks',
-    facebook: 'https://facebook.com',
-    youtube: 'https://youtube.com/codebucks',
-    linkedin: 'https://www.linkedin.com/in/codebucks/',
-    dribbble: 'https://www.dribbble.com',
+    email: 'zulugeoffrey034@gmail.com', 
+    github: 'https://github.com/Geoffrey-Zulu',
+    twitter: 'https://twitter.com/geoffreyzulujr',
+    facebook: 'https://facebook.com/geoffreyzulujr',
+    youtube: 'https://youtube.com/geoffreyzulujr',
+    linkedin: 'https://www.linkedin.com/in/geoffreyzulujr/',
+    dribbble: 'https://www.dribbble.com/geoffreyzulujr',
     locale: 'en-US',
   }
   
