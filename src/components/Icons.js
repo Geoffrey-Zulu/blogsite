12e5e9f33dda36ef2ed732325a2e2299b90114ff
Geoffrey-Zulu <76912380+Geoffrey-Zulu@users.noreply.github.com>
2024-09-08 01:51:10 +0200

    Update Icons.js

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -437,3 +437,23 @@ export const DribbbleIcon = ({ className, ...rest }) => {
     </svg>
   );
 };
+
+export const InstagramIcon = ({ className, ...rest }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className={cx("w-full h-auto", className)}
+    {...rest}
+  >
+    <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
+    <path d="M16 11.37a4 4 0 1 1-4.37-4.37 4 4 0 0 1 4.37 4.37z"></path>
+    <line x1="17.5" y1="6.5" x2="17.5" y2="6.5"></line>
+  </svg>
+);
